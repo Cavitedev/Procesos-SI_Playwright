685ebd205e2ac828a2e358000b4d45b84fb6c9c1
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-01-04 11:22:19 +0100

    Arreglar test ahora que salir de una partida cierra ambas sesiones

--- tests/barcos.spec.js
+++ tests/barcos.spec.js
@@ -98,6 +98,5 @@ test("test", async ({ browser }) => {
     .locator("div:nth-child(2) > .grid > div:nth-child(56)")
     .click();
 
-  await pepePage.getByRole("button", { name: "Cerrar Sesión" }).click();
   await juanPage.getByRole("button", { name: "Cerrar Sesión" }).click();
 });
